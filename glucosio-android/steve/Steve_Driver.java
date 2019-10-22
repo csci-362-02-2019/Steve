dc8ca24a29f9bb237949020dbcfd2b2c9b6a368e
Michael <54380156+Ocainmm@users.noreply.github.com>
2019-10-22 11:30:22 -0400

    overwrote steve folder
    fixed driver made copy of GlucosioConverter in steve

--- glucosio-android/steve/Steve_Driver.java
+++ glucosio-android/steve/Steve_Driver.java
@@ -5,59 +5,58 @@
  * @author Boop
  * @version 
  */
-public class Steve_Driver
-{
-    
-    
+public class Steve_Driver {
+	    
 
-    /**
-     * Constructor for objects of class Steve_Driver
-     */
-    public Steve_Driver()
-    {
-        
-    }
+	    /**
+	     * Constructor for objects of class Steve_Driver
+	     */
+	    public Steve_Driver()
+	    {
+	        
+	    }
 
-    public static void main(String args[]){
-    
-        String methodName = args[0];
-        double argument = Double.parseDouble(args[1]);
-        double out = -1.0;
-        
-        switch (methodName) {
-            
-            default:
-            
-                out = GlucosioConverter.round(argument);
-                break;
-                
-            case "round":
-            
-                out = GlucosioConverter.round(argument);
-                break;
-                
-            case "glucoseToMgDl":
-            
-                out = GlucosioConverter.glucoseToMgDl(argument);
-                break;
-                
-            case "glucoseToMmolL":
-            
-                out = GlucosioConverter.glucoseToMmolL(argument);
-                break;
-                
-            case "glucoseToA1C": 
-            
-                out = GlucosioConverter.glucoseToMmolL(argument);
-                break;
-                
-            case "a1cToGlucose":
-            
-                out = GlucosioConverter.glucosea1cToGlucose(argument);
-                break;
-        
-            }
-            
-            System.out.print(out);
-    }
+	    public static void main(String args[]){
+	    
+	        String methodName = args[0];
+	        double argument = Double.parseDouble(args[1]);
+	        double out = -1.0;
+	        int argument2;
+	        switch (methodName) {
+	            
+	            default:
+	            	argument2=Integer.parseInt(args[2]);
+	                out = GlucosioConverter.round(argument,argument2);
+	                break;
+	                
+	            case "round":
+	            	argument2=Integer.parseInt(args[2]);
+	                out = GlucosioConverter.round(argument,argument2);
+	                break;
+	                
+	            case "glucoseToMgDl":
+	            
+	                out = GlucosioConverter.glucoseToMgDl(argument);
+	                break;
+	                
+	            case "glucoseToMmolL":
+	            
+	                out = GlucosioConverter.glucoseToMmolL(argument);
+	                break;
+	                
+	            case "glucoseToA1C": 
+	            
+	                out = GlucosioConverter.glucoseToMmolL(argument);
+	                break;
+	                
+	            case "a1cToGlucose":
+	            
+	                out = GlucosioConverter.a1cToGlucose(argument);
+	                break;
+	        
+	            }
+	            
+	            System.out.print(out);
+	    }
 }
+
