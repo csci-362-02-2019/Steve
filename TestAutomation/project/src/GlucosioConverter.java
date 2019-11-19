1bb4b07ff701eded341394565c207fac38286a14
Ocainmm <54380156+Ocainmm@users.noreply.github.com>
2019-11-19 11:19:46 -0500

    Update GlucosioConverter.java

--- TestAutomation/project/src/GlucosioConverter.java
+++ TestAutomation/project/src/GlucosioConverter.java
@@ -31,11 +31,11 @@ public final class GlucosioConverter {
     private GlucosioConverter() {
     }
 
-    public static double round(double value, double places) {
+    public static double round(double value, int places) {
         if (places < 0) throw new IllegalArgumentException();
 
         BigDecimal bd = BigDecimal.valueOf(value);
-        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        bd = bd.setScale(places, RoundingMode.HALF_DOWN);
         return bd.doubleValue();
     }
 
