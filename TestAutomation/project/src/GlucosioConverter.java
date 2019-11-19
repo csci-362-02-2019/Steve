2d4a150f440346e71773407a08fb94a9dcb017bb
Ocainmm <54380156+Ocainmm@users.noreply.github.com>
2019-11-19 11:26:16 -0500

    Update GlucosioConverter.java

--- TestAutomation/project/src/GlucosioConverter.java
+++ TestAutomation/project/src/GlucosioConverter.java
@@ -44,7 +44,7 @@ public final class GlucosioConverter {
     }
 
     public static double glucoseToMmolL(double mgDl) {
-        return round(mgDl / MG_DL_TO_MMOL_CONSTANT, 2);
+        return round(mgDl / MG_DL_TO_MMOL_CONSTANT, 3);
     }
 
     public static double glucoseToA1C(double mgDl) {
