332e42b8992d79d5881fc4e389cde445d9f2e296
Ocainmm <54380156+Ocainmm@users.noreply.github.com>
2019-11-20 20:39:59 -0500

    Update GlucosioConverterF.java

--- TestAutomation/project/src/GlucosioConverterF.java
+++ TestAutomation/project/src/GlucosioConverterF.java
@@ -23,12 +23,12 @@
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-public final class GlucosioConverter {
+public final class GlucosioConverterf {
 
     private static final double MG_DL_TO_MMOL_CONSTANT = 18.0;
     private static final double KG_TO_LB_CONSTANT = 2.20462;
 
-    private GlucosioConverter() {
+    private GlucosioConverterF() {
     }
 
     public static double round(double value, int places) {
